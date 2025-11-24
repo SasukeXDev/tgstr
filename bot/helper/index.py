b63cd2cec4a9c914953e0fa9685dd4a221bd6ab3
SasukeXDev <srk100m@gmail.com>
2025-11-24 23:33:55 +0530

    Update index.py
    f

--- bot/helper/index.py
+++ bot/helper/index.py
@@ -28,7 +28,7 @@ async def get_messages(chat_id, first_message_id, last_message_id, batch_size=50
         for message in batch_messages:
             if message:
                 if file := message.video or message.document:
-                    title = file.file_name or message.caption or file.file_id
+                    title = post.caption    #file.file_name or message.caption or file.file_id
                     title, _ = splitext(title)
                     title = re.sub(r'[.,|_\',]', ' ', title)
                     messages.append({"msg_id": message.id, "title": title,
