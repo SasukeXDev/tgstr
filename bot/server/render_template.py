c22724ad7099eaac2df7fe8b7898e56f257716fc
SasukeXDev <srk100m@gmail.com>
2025-11-25 02:33:03 +0530

    Update render_template.py
    Hehe

--- bot/server/render_template.py
+++ bot/server/render_template.py
@@ -107,9 +107,8 @@ async def render_page(
             )
             LOGGER.info("Invalid hash for message with - ID %s", id)
             raise InvalidHash
-        filename, caption, tag, size = (
+        filename, tag, size = (
             file_data.file_name,
-            file_data.message_caption,
             file_data.mime_type.split("/")[0].strip(),
             get_readable_file_size(file_data.file_size),
         )
